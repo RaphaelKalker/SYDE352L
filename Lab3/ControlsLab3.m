4f9d953fc0240825e3f4644d883844cbd18b3653
Raphael Kalker <raphael.kalker@gmail.com>
2015-03-11 12:28:13 -0400

    removed commented out code, added plots of zeroes and poles

--- Lab3/ControlsLab3.m
+++ Lab3/ControlsLab3.m
@@ -7,72 +7,49 @@ function main()
     TF1_A = Tfs(1);
     TF2_A = Tfs(2);
     [p1A, z1A] = pzmap(TF1_A)
-    [p2A, z2A] = pzmap(TF1_A)
+    [p2A, z2A] = pzmap(TF2_A)
+    subplot(221)
+    pzplot(TF1_A)
+    subplot(222)
+    pzplot(TF2_A)
     
+    
+    clear;
     kp = .21; %step 5
     kd = .042; %step 5
     Tfs = calcTF(kp, kd);
     TF1_B = Tfs(1);
     TF2_B = Tfs(2);
     [p1B, z1B] = pzmap(TF1_B)
-    [p2B, z1B] = pzmap(TF2_B)
+    [p2B, z2B] = pzmap(TF2_B)
+    subplot(223)
+    pzplot(TF1_B)
+    subplot(224)
+    pzplot(TF2_B)
 
     nr = 220;
     count = 0;
-
+    
     %Plot root loci according to ratios
+    figure;
     for ratio = [.05, 0.10, .17, .25]
-        %step 3
-        Tfs = calcTF(ratio);
-        TF1_A = Tfs(1);
-        TF2_A = Tfs(2);
+%     for ratio = [.3, 1]
+        Tfs = calcTF(ratio);        
+        TF1 = Tfs(1);
+        TF2 = Tfs(2);
         
         %Get Poles, Zeroes, Root Loci
         if count == 4
            count = 0; 
            figure;
         end
+        
         count = count +1 ;
-        rootloci(TF1_A, nr + count);
+        rootloci(TF1, nr + count);
         count = count +1;
-        rootloci(TF2_A, nr + count);
+        rootloci(TF2, nr + count);
     end
 end
-
-% function TransferFunctions = calcTF(varargin)
-%     %nargin = 2 - kp,kd given
-%     %nargin = 1 - ratio
-%     
-%     if nargin == 2
-%         kp = varargin{1};
-%         kd = varargin{2};
-%     else
-%         if nargin == 1
-%         ratio = varargin{1};
-%         kp = ratio;
-%         %ratio
-%         end
-%     end
-%         
-%     s = tf('s');
-% 
-%     khw = 16.1000; %from parameters reference
-%     m1 = 522.2 + 492.5 + 492.8 + 494.7 + 809.1;
-%     m1 = m1/1000;
-%     m2 = 490.9 + 494.9 + 496.0 + 245.0 + 656.5;
-%     m2 = m2/1000;
-%     k2 = 426.2150; %from parameters reference
-% 
-%     Ds =(m1*m2)*s^4 + (m1*(k2) + m2*(k2))*s^2;
-%     N1 = m2*s^2 + k2;
-%     N2 = k2;
-%     G1 = (kp + kd*s)*khw * N1/Ds;
-%     G2 = (kp + kd*s)*khw * N2/Ds;
-%     TF1 = G1/(1+G1);
-%     TF2 = G2/(1+G2);
-%     %changed
-%     TransferFunctions = [G1 G2];
-% end
 
 function OpenLoopTransferFunctions = calcTF(varargin)
     %nargin = 2 - kp,kd given
